f5ec73670ce3ab4a8b61ba29ec38d522793a12ae
Madan939 <madantamang939@gmail.com>
2024-08-05 14:20:42 +0545

    update

--- Frontend/src/routes/MyRoute.js
+++ Frontend/src/routes/MyRoute.js
@@ -39,41 +39,50 @@ const MyRoute = () => {
         <Route path="/adminlogin" element={<Adminlogin />} />
         <Route path="/adminsignup" element={<Adminregister />} />
         {/* <Route path="/admin/admin-homepage" element={<ProtectedRoute><Home /></ProtectedRoute>} /> */}
-        <Route path="/admin/add-course" element={<ProtectedRoute><Addcourse /></ProtectedRoute>} />
-        <Route path="/admin/add-subject" element={<ProtectedRoute><Addsubject /></ProtectedRoute>} />
-        <Route path="/admin/edit-subject/:_id" element={<ProtectedRoute><Editsubject /></ProtectedRoute>} />
-        <Route path="/admin/add-student" element={<ProtectedRoute><Addstudent /></ProtectedRoute>} />
-        <Route path="/admin/add-result" element={<ProtectedRoute><Addresult /></ProtectedRoute>} />
-        <Route path="/admin/edit-notice/:_id" element={<ProtectedRoute><Editnotice /></ProtectedRoute>} />
-        <Route path="/admin/add-notice" element={<ProtectedRoute><Addnotice /></ProtectedRoute>} />
-        <Route path="/admin/admin-notice" element={<ProtectedRoute><Adminnotice /></ProtectedRoute>} />
-        <Route path="/admin/semesterlist" element={<ProtectedRoute><Semesterlist /></ProtectedRoute>} />
-        <Route path="/admin/schoolarships-details" element={<ProtectedRoute><Schoolarshipdetails /></ProtectedRoute>} />
-        <Route path="/admin/studentlist" element={<ProtectedRoute><ShowStudent /></ProtectedRoute>} />
-        <Route path="/admin/view-student/:_id" element={<ProtectedRoute><ViewStudent /></ProtectedRoute>} />
-        <Route path="/admin/edit-student/:_id" element={<ProtectedRoute><EditStudent /></ProtectedRoute>} />
+        <Route path="/admin/add-course" element={<ProtectedRoute><AdminRoute><Addcourse /></AdminRoute></ProtectedRoute>} />
+        <Route path="/admin/add-subject" element={<ProtectedRoute><AdminRoute><Addsubject /></AdminRoute></ProtectedRoute>} />
+        <Route path="/admin/edit-subject/:_id" element={<ProtectedRoute><AdminRoute><Editsubject /></AdminRoute></ProtectedRoute>} />
+        <Route path="/admin/add-student" element={<ProtectedRoute><AdminRoute><Addstudent /></AdminRoute></ProtectedRoute>} />
+        <Route path="/admin/add-result" element={<ProtectedRoute><AdminRoute><Addresult /></AdminRoute></ProtectedRoute>} />
+        <Route path="/admin/edit-notice/:_id" element={<ProtectedRoute><AdminRoute><Editnotice /></AdminRoute></ProtectedRoute>} />
+        <Route path="/admin/add-notice" element={<ProtectedRoute><AdminRoute><Addnotice /></AdminRoute></ProtectedRoute>} />
+        <Route path="/admin/admin-notice" element={<ProtectedRoute><AdminRoute><Adminnotice /></AdminRoute></ProtectedRoute>} />
+        <Route path="/admin/semesterlist" element={<ProtectedRoute><AdminRoute><Semesterlist /></AdminRoute></ProtectedRoute>} />
+        <Route path="/admin/schoolarships-details" element={<ProtectedRoute><AdminRoute><Schoolarshipdetails /></AdminRoute></ProtectedRoute>} />
+        <Route path="/admin/studentlist" element={<ProtectedRoute><AdminRoute><ShowStudent /></AdminRoute></ProtectedRoute>} />
+        <Route path="/admin/view-student/:_id" element={<ProtectedRoute><AdminRoute><ViewStudent /></AdminRoute></ProtectedRoute>} />
+        <Route path="/admin/edit-student/:_id" element={<ProtectedRoute><AdminRoute><EditStudent /></AdminRoute></ProtectedRoute>} />
         <Route path="/fac-sub" element={<ProtectedRoute><Facsub /></ProtectedRoute>} />
-        <Route path="/admin/show-result" element={<ProtectedRoute><Showresult /></ProtectedRoute>} />
-        <Route path="/admin/view-result/:_id" element={<ProtectedRoute><Viewresult /></ProtectedRoute>} />
-        <Route path="/admin/edit-result/:_id" element={<ProtectedRoute><EditResult /></ProtectedRoute>} />
+        <Route path="/admin/show-result" element={<ProtectedRoute><AdminRoute><Showresult /></AdminRoute></ProtectedRoute>} />
+        <Route path="/admin/view-result/:_id" element={<ProtectedRoute><AdminRoute><Viewresult /></AdminRoute></ProtectedRoute>} />
+        <Route path="/admin/edit-result/:_id" element={<ProtectedRoute><AdminRoute><EditResult /></AdminRoute></ProtectedRoute>} />
         <Route path="/confirmation/:token" element={<Confirmation />} />
         <Route path="/forgotpassword" element={<Forgetpassword />} />
         <Route path="/confirmreset" element={<Confirmreset />} />
         <Route path="/resetpassword/:token/:_id" element={<Resetpassword />} />
         <Route path="*" element={<PageNotFound />} />
-        <Route path="/" element={<ProtectedRoute><HomePage/></ProtectedRoute>} />
-        <Route path="/access" element={<ProtectedRoute><Access/></ProtectedRoute>} />
+        <Route path="/" element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
+        <Route path="/access" element={<ProtectedRoute><AdminRoute><Access /></AdminRoute></ProtectedRoute>} />
       </Routes>
     </>
   )
 }
 
-export default MyRoute
+export default MyRoute;
 export function ProtectedRoute({ children }) {
   if (localStorage.getItem('Admin')) {
     return children
   }
   else {
     return <Navigate to="/adminlogin" />
+  }
+};
+export function AdminRoute({ children }) {
+  const login = JSON.parse(localStorage.getItem('Admin'))
+  if (login.User.role === "admin") {
+    return children
+  }
+  else {
+    return <Navigate to="*" />
   }
 }
